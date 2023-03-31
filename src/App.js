301505b9196d98c47c247fb1180b0f0630aaef45
Pohlsj <Pohlsj2@gmail.com>
2023-03-31 11:11:32 -0700

    the start to something

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+// -- COMPONENT IMPORTS --
+import Header from "./Components/Header/Header.js";
+
+// -- STYLE IMPORTS --
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <body>
+        <header className="App-header">L&S Clothing</header>
+      </body>
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+// -- MODULE IMPORTS --
+import React from "react";
+import { Typography } from "@mui/material";
+
+// -- STYLE IMPORTS --
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="LS-header">
+      <Typography className="Logo">L&S Clothing</Typography>
+    </div>
+  );
+};
+
+export default Header;
